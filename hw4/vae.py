83ba2abfbc1cacef5fb6e69d4887835cd24947f6
andrewkuo <andrewkuogs@gmail.com>
2018-05-21 20:45:34 +0800

    fix matplotlib problem

--- hw4/vae.py
+++ hw4/vae.py
@@ -3,6 +3,8 @@ import os
 import argparse
 import numpy as np
 import tensorflow as tf
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
